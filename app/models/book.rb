7fdff55f2859c26c792310e983beffe942863840
Mateusz Miodek <mmiodek@gmail.com>
2017-04-23 16:58:12 +0200

    initial search - title only

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,13 +1,10 @@
 class Book < ApplicationRecord
   scope :finished, ->{ where.not(finished_on: nil)}
   scope :recent, ->{ where('finished_on > ?', 2.days.ago)}
+  scope :search, ->(keyword){where(title: keyword) if keyword.present?}
 
   def finished?
     finished_on.present?
-  end
-
-  def has_author?
-    author.present?
   end
 
   def rating_descr

0842e421f0a2560dc83b0651488bf87f52d4b81a
Mateusz Miodek <mmiodek@gmail.com>
2017-04-29 01:10:30 +0200

    added genres

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,10 @@
 class Book < ApplicationRecord
+  has_many :book_genres
+  has_many :genres, through: :book_genres
   scope :finished, ->{ where.not(finished_on: nil)}
   scope :recent, ->{ where('finished_on > ?', 2.days.ago)}
   scope :search, ->(keyword){where('keywords Like ?',"%#{keyword.downcase}%") if keyword.present?}
+  scope :filter_by_genre, ->(name){ joins(:genres).where('genres.name = ?', name) if name.present?}
 
   before_save :set_keywords
 
@@ -22,7 +25,11 @@ class Book < ApplicationRecord
 
   protected
     def set_keywords
-      self.keywords = [title, author].map(&:downcase).join(' ')
+      book_genres = ''
+      self.genres.each do |g|
+        books_genres << g.name + ',' 
+      end
+      self.keywords = [title, author, books_genres].map(&:downcase).join(' ')
     end
 
 end
